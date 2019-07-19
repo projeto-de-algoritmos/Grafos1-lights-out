2d1344fcf4c3207e0cd2688edca7d5f9d5bccd4e
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-07-18 23:54:54 -0300

    Add inital home screen component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import Board from './Components/Board/index'
-
+import Home from './Components/Home/index'
+/* <Board rows={5} cols={5} changeLightStartOn={.3}/> */
 function App() {
-  return <Board rows={5} cols={5} changeLightStartOn={.3}/>
+  return <Home/>
 }
 
 export default App;
---
+++ src/Components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+
+const Home = () => {
+
+  return (
+    <h1>Home Screen</h1>
+  )
+}
+
+export default Home
