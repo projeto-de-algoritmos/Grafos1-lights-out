bef2f6a6f243b45a83077dded622ac1ab18158d1
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-07-21 19:01:26 -0300

    Improve board colors and animate it when hovering

--- src/Components/Board/index.js
+++ src/Components/Board/index.js
@@ -11,9 +11,7 @@ const Board = ({ rows, cols, changeLightStartOn }) => {
   const [hasWon, setHasWon] = React.useState(false)
 
   const handleCellClick = (board, rows, cols, x, y) => {
-
     setBoard(toggleAdjCells(board, rows, cols, x, y))
-
     const hasWon = board.every(arr => arr.every(val => !val))
     hasWon && setTimeout(() => setHasWon(true), 250)
   }
@@ -21,11 +19,13 @@ const Board = ({ rows, cols, changeLightStartOn }) => {
   return (
     <>
       { hasWon && <NeonText first="You" second="Won"/> }
-      <table className="board">
+      <table className="board"cellSpacing="4">
         <tbody>
           {board.map((arr, x) => 
             <tr key={ x }>
-              { arr.map((val, y) => <Cell key={ `${x}-${y}` } isLit={ val } toggleCell={ () => handleCellClick(board, rows, cols, x, y) }/>) }
+              { arr.map((val, y) => 
+                  <Cell key={ `${x}-${y}` } isLit={ val } 
+                        toggleCell={ () => handleCellClick(board, rows, cols, x, y) }/>) }
             </tr>
           )}
         </tbody>
