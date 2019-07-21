fc02838de639a556cebfd6c0c9731dbe72d4c92b
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-07-21 01:12:49 -0300

    Add UI feedback for the form when logging in

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import './index.css'
+import classNames from 'classnames'
 import NeonText from '../NeonText/index'
 import Login from '../Login/index'
+import './index.css'
 
 const Home = () => {
   const [authType, setAuthType] = React.useState(null)
@@ -10,13 +11,21 @@ const Home = () => {
 
   const isLoginSelected = () => authType === 'login'
   const isSignUpSelected = () => authType === 'signup'
+  const getMethodForLogin = () => isLoginSelected() ? () => authLogin() : () => {setAuthType('login'); setEmail(''); setPassword('');}
+
+  const authLogin = () => {
+    const user = { email, password }
+    console.log(user)
+  }
 
   return (
     <main>
-      {/* <NeonText first="Lights" second="Out"/> */}
-      {isLoginSelected() ? <Login setEmail={ setEmail } setPassword={ setPassword }/> : null}
+      <NeonText first="Lights" second="Out"/>
+      { isLoginSelected() ? <Login email={email} password={password} setEmail={setEmail} setPassword={setPassword}/> : null }
       <div className="home-auth-selection">
-        <button className="btn btn-primary" onClick={() => setAuthType('login')}>{isLoginSelected() ? 'Enter' : 'Login'}</button>
+        <button className={ classNames('btn', 'btn-primary', {'disabled': isLoginSelected() && (!email || !password)}) }
+                onClick={ getMethodForLogin() }> {isLoginSelected() ? 'Enter' : 'Login'} </button>
+                
         <button className="btn btn-secondary" onClick={() => setAuthType('signup')}>Sign Up</button>
       </div>
     </main>
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -3,16 +3,19 @@ import './index.css'
 
 const Login = ({ email, password, setEmail, setPassword}) => (
   <form>
-    <h1>Enter your data</h1>
-    <label>
-      Email
-      <input value={ email } onChange={e => setEmail(e.target.value) }/>
-    </label>
-    
-    <label>
-      Password
-      <input value={ password } onChange={ e => setPassword(e.target.value) }/>
-    </label>
+    {console.log(email)}
+    <h1>Login</h1>
+    <div className="labels-container">
+      <label>
+        Email
+        <input className={email ? "valid" : null} value={ email } onChange={e => setEmail(e.target.value) }/>
+      </label>
+      
+      <label>
+        Password
+        <input className={password ? "valid" : null} value={ email } value={ password } onChange={ e => setPassword(e.target.value) }/>
+      </label>
+    </div>
   </form>
 )
 
