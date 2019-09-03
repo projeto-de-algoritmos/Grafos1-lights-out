481a777b69c6970db444fc014190502c7237d0a5
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-03 14:07:41 -0300

    Deploy to Github Pages

--- src/Components/Cell/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './style.css'
-
-const Cell = ({ isLit, toggleCell, player1 }) => <td className={"cell" + (isLit ? (player1 ? " cell-lit-orange" : " cell-lit-blue") : "")}
-  onClick={toggleCell} />
-
-export default Cell
